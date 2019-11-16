999824c1b4169eca4f63e878ff00bf2e2c0a39bc
florian <b.florian91@icloud.com>
2019-11-16 16:09:57 +0100

    save photo undeconstruc

--- view/MontagePageView.php
+++ view/MontagePageView.php
@@ -7,11 +7,11 @@
     ?>
     <div class="container col" style="margin-top: 150px">
       <div class="row justify-content-center">
-        <div class="col-md-6 ml-4" style="min-height: 50vh; border: solid 1px">
-          <video class="w-100" id="video"></video>
+        <div class="col-md-6 ml-4" style="min-height: 80vh; border: solid 1px">
+          <video class="w-100 position-relative" id="video" playinline autoplay ></video>
           <canvas class="w-100" id="canvas"></canvas>
         </div>
-        <div class="col-md-2 ml-4" style="min-height: 50vh; border: solid 1px;">
+        <div class="col-md-2 ml-4" style="min-height: 80vh; border: solid 1px;">
           <h1 class="text-center" >Options</h1>
           <hr>
           <div class="row justify-content-center mb-4">
@@ -23,6 +23,7 @@
             <img width="100" src="../lib/filtre/lu1.png" alt="magi">
           </div>
           <button class="w-100 py-2 mb-4" id="startButton">Take photo</button> 
+          <a href="#" id="dl-btn" download="lol.png"><button class="w-100 py-2 mb-4" id="saveButton">Save photo</button></a>
           <button class="w-100 py-2" id="clearButton">Clear photo</button> 
         </div>
       </div>
