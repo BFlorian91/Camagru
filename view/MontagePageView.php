92f9c04f7f6d015ac73304021b80da910ecedfe1
florian <b.florian91@icloud.com>
2019-11-16 03:50:25 +0100

    fake add filter for testing

--- view/MontagePageView.php
+++ view/MontagePageView.php
@@ -6,19 +6,24 @@
     {
     ?>
     <div class="container col" style="margin-top: 150px">
-      <div class="row justify-content-around">
-        <div class="col-md-6" style="min-height: 70vh; border: solid 1px">
-          <div class="camera">
-            <video id="video"></video>
-            <canvas id="canvas"></canvas>
-            <!-- <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"> -->
+      <div class="row justify-content-center">
+        <div class="col-md-6 ml-4" style="min-height: 50vh; border: solid 1px">
+          <video class="w-100" id="video"></video>
+          <canvas class="w-100" id="canvas"></canvas>
+        </div>
+        <div class="col-md-2 ml-4" style="min-height: 50vh; border: solid 1px;">
+          <h1 class="text-center" >Options</h1>
+          <hr>
+          <div class="row justify-content-center mb-4">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
+            <img width="100" src="../lib/filtre/lu1.png" alt="magi">
           </div>
-          <div class="col-md-3" style="min-height: 70vh; border: solid 1px;">
-            <h1>Options</h1>
-            <hr>
-           <button class="w-100 py-2 mb-4" id="startButton">Take photo</button> 
-           <button class="w-100 py-2" id="clearButton">Clear photo</button> 
-          </div>
+          <button class="w-100 py-2 mb-4" id="startButton">Take photo</button> 
+          <button class="w-100 py-2" id="clearButton">Clear photo</button> 
         </div>
       </div>
     </div>
