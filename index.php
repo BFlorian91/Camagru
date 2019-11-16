ad560c33d656ea854e9a44bf4c89042e9720089c
florian <b.florian91@icloud.com>
2019-11-16 03:05:32 +0100

    camera seems work need to be upgraded

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     require_once 'view/View.php';
     require_once 'view/SignupView.php';
     require_once 'view/EditAccountView.php';
+    require_once 'view/MontagePageView.php';
     require_once 'connectToBdd.php';
     require_once 'lib/DataUserRecord.php';
     require_once 'lib/SqlStatement/Sqlstatement.php';
@@ -11,7 +12,7 @@
     require_once 'Ctrl/Ctrl.php';
     require_once 'Ctrl/CtrlSignUp.php';
 
-    $ctrl = Ctrl::chooseCtrl();
-    $ctrl->start();
-    echo $ctrl->getView();
+    // $ctrl = Ctrl::chooseCtrl();
+    $ctrl = new MontagePage;
+    echo $ctrl->buildPage();
 ?>
--- view/MontagePageView.php
+++ view/MontagePageView.php
@@ -5,27 +5,29 @@
     public function bodyPage()
     {
     ?>
-    <div class="container col bg-primary" style="margin-top: 150px">
+    <div class="container col" style="margin-top: 150px">
       <div class="row justify-content-around">
-        <div class="col-md-8 bg-warning" style="min-height: 70vh">
-        <div>
-          <script>
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-          </script>
+        <div class="col-md-8" style="min-height: 70vh; border: solid 1px">
+        <div class="camera">
+         <video id="video">Video stream not available.</video>
+        </div>
+        <canvas id="canvas"></canvas>
+        <div class="output">
+          <img id="photo" alt="The screen capture will appear in this box."> 
         </div>
         </div>
-        <div class="col-md-3 bg-success" style="min-height: 70vh">
+        <div class="col-md-3" style="min-height: 70vh; border: solid 1px;">
           <h1>Options</h1>
           <hr>
           <div class="w-75 bg-danger">
 
           </div>
-          <form action="#" method="post">
-            <input type="submit" class="w-100" style="height: 4em;" name="takePhoto" value="Take Photo">
-          </form>
+           <button class="w-100 py-2 mb-4" id="startButton">Take photo</button> 
+           <button class="w-100 py-2" id="clearButton">Clear photo</button> 
         </div>
       </div>
     </div>
+    <script src="../lib/js/camera.js"></script>
 <?php
     }
   }
