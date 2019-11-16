cbacee898827313037274d7d85598fb42a317c53
florian <b.florian91@icloud.com>
2019-11-16 03:29:46 +0100

    update camera | photo replace the video when click on take picture and reload video when click on clear photo

--- view/MontagePageView.php
+++ view/MontagePageView.php
@@ -7,23 +7,18 @@
     ?>
     <div class="container col" style="margin-top: 150px">
       <div class="row justify-content-around">
-        <div class="col-md-8" style="min-height: 70vh; border: solid 1px">
-        <div class="camera">
-         <video id="video">Video stream not available.</video>
-        </div>
-        <canvas id="canvas"></canvas>
-        <div class="output">
-          <img id="photo" alt="The screen capture will appear in this box."> 
-        </div>
-        </div>
-        <div class="col-md-3" style="min-height: 70vh; border: solid 1px;">
-          <h1>Options</h1>
-          <hr>
-          <div class="w-75 bg-danger">
-
+        <div class="col-md-6" style="min-height: 70vh; border: solid 1px">
+          <div class="camera">
+            <video id="video"></video>
+            <canvas id="canvas"></canvas>
+            <!-- <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"> -->
           </div>
+          <div class="col-md-3" style="min-height: 70vh; border: solid 1px;">
+            <h1>Options</h1>
+            <hr>
            <button class="w-100 py-2 mb-4" id="startButton">Take photo</button> 
            <button class="w-100 py-2" id="clearButton">Clear photo</button> 
+          </div>
         </div>
       </div>
     </div>
