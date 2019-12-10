3c474e8fd11d96d6fcb814566ed7c8cc06faf2f4
Tbeaumont79 <tbeaumont79@icloud.com>
2019-12-10 14:51:00 +0100

    fix the view

--- view/GalleryView.php
+++ view/GalleryView.php
@@ -12,20 +12,27 @@ class Gallery extends View
     <div class="container">
       <h1 class="text-center">Home</h1>
       <?php
-
+      foreach ($this->_data as $key => $values) {
+        if ($key == 'images') {
           echo '<form method="post">
-            <img name="' . $this->_data['images'] . ' "src="' . $this->_data['images'] . '">              
-              <button type="submit" name="like">Like</button>
-                  <p>' . $this->_data['likes'] . '<p>
-                  <p>' . $this->_data['username'] . '</p>
-                  ';
-          foreach ($this->_data['commentAndDate'] as $value) {
-            echo '<p>' . $value . '</p>';
-          }
-          echo '
-              <textarea name="comment" cols="100" rows="5" placeholder="had a comment"></textarea>
-              <button type="submit" class="btn btn-primmary">Send</button>
-            </form>';
+          <img "src ='.$values.'">              
+        ';
+        debug($values);
+        }
+    }
+      echo '
+      <button type="submit" name="like">Like</button>
+              <p>' . $this->_data['likes'] . '<p>
+              <p>' . $this->_data['username'] . '</p>
+              ';
+      foreach ($this->_data['commentAndDate'] as $value) {
+        echo '<p>' . $value . '</p>';
+      }
+      echo '
+          <textarea name="comment" cols="100" rows="5" placeholder="had a comment"></textarea>
+          <button type="submit" class="btn btn-primmary">Send</button>
+        </form>';
+    
           ?>
     </div>
 <?php
